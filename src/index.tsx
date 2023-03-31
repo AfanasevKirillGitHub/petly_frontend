6eb89a625f961ab070e474771c711d7124eccbab
SPoluliakh <S_Poluliakh@ukr.net>
2023-03-31 20:20:29 +0300

    added dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from './components/App/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
