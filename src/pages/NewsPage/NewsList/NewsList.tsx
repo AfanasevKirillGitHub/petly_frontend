a0887a47e099ac94370d52bd250ac3ec317e48ac
SPoluliakh <S_Poluliakh@ukr.net>
2023-04-02 10:58:51 +0300

    added change color func

--- src/pages/NewsPage/NewsList/NewsList.tsx
+++ src/pages/NewsPage/NewsList/NewsList.tsx
@@ -1,32 +1,51 @@
+import { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useFetchNewsQuery } from '../../../redux/news/newsOperations';
 import { useGetSearchParams } from '../../../huks/useGetSearchParams';
+import { Hightlight } from '../../../components/Hightlight/Hightlight';
+import { Loader } from '../../../components/Loader/Loader';
 
 export const NewsList = () => {
   const { t } = useTranslation();
   const { lang, keyword } = useGetSearchParams();
-  const { data } = useFetchNewsQuery(
+  const { data, isFetching } = useFetchNewsQuery(
     { lang, keyword },
     {
       refetchOnMountOrArgChange: true,
     }
   );
 
-  console.log(keyword);
+  const ChangeColor = useCallback(
+    (str: string) => {
+      return <Hightlight filter={keyword} str={str} />;
+    },
+    [keyword]
+  );
 
   return (
-    <ul>
-      {data?.map(({ title, date, description, link, _id, img }) => (
-        <li key={_id}>
-          <h3>{title['en'] ? title['en'] : title['ua']}</h3>
-          <img src={img} alt={title['en'] ? title['en'] : title['ua']} />
-          <p>{description['en'] ? description['en'] : description['ua']}</p>
-          <a href={link} target="_blank" rel="noopener noreferrer">
-            {t('Read more')}
-          </a>
-          <p>{date}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      {isFetching && <Loader />}
+      <ul>
+        {data?.map(({ title, date, description, link, _id, img }) => (
+          <li key={_id}>
+            <h3>
+              {ChangeColor((title['en'] ? title['en'] : title['ua']) as string)}
+            </h3>
+            <img src={img} alt={title['en'] ? title['en'] : title['ua']} />
+            <p>
+              {ChangeColor(
+                (description['en']
+                  ? description['en']
+                  : description['ua']) as string
+              )}
+            </p>
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              {t('Read more')}
+            </a>
+            <p>{date}</p>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
