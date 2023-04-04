d85de66831354ba762142d4788c6f9b32ee2bd5f
SPoluliakh <S_Poluliakh@ukr.net>
2023-04-04 18:35:13 +0300

    added all routs

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -38,13 +38,58 @@ const FriendsPage = lazy(() =>
   }))
 );
 
+const LostFoundPage = lazy(() =>
+  import('../../pages/FindPetPage/LostFoundPage/LostFoundPage').then(
+    module => ({
+      ...module,
+      default: module.LostFoundPage,
+    })
+  )
+);
+
+const InGoodHandPage = lazy(() =>
+  import('../../pages/FindPetPage/InGoodHandPage/InGoodHandPage').then(
+    module => ({
+      ...module,
+      default: module.InGoodHandPage,
+    })
+  )
+);
+
+const FavoritePage = lazy(() =>
+  import('../../pages/FindPetPage/FavoritePage/FavoritePage').then(module => ({
+    ...module,
+    default: module.FavoritePage,
+  }))
+);
+
+const MyAdsPage = lazy(() =>
+  import('../../pages/FindPetPage/MyAdsPage/MyAdsPage').then(module => ({
+    ...module,
+    default: module.MyAdsPage,
+  }))
+);
+
+const SellPage = lazy(() =>
+  import('../../pages/FindPetPage/SellPage/SellPage').then(module => ({
+    ...module,
+    default: module.SellPage,
+  }))
+);
+
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Navigate to="news" />} />
         <Route path="/news" element={<NewsPage />} />
-        <Route path="/find-pet" element={<FindPetPage />} />
+        <Route path="/find-pet" element={<FindPetPage />}>
+          <Route path="lost-found" element={<LostFoundPage />} />
+          <Route path="good-hand" element={<InGoodHandPage />} />
+          <Route path="sell" element={<SellPage />} />
+          <Route path="favorite" element={<FavoritePage />} />
+          <Route path="my-ads" element={<MyAdsPage />} />
+        </Route>
         <Route path="/friends" element={<FriendsPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/registration" element={<RegisterPage />} />
