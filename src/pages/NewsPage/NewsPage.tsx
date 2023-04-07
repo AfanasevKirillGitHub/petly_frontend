e526cc9bb50157598b807c691f0197b332b855b5
SPoluliakh <S_Poluliakh@ukr.net>
2023-04-07 19:39:14 +0300

    added styles

--- src/pages/NewsPage/NewsPage.tsx
+++ src/pages/NewsPage/NewsPage.tsx
@@ -1,14 +1,15 @@
 import { useTranslation } from 'react-i18next';
 import { NewsList } from './NewsList/NewsList';
 import { SearchBar } from '../../components/SearchBar/SearchBar';
+import * as SC from './NewsPage.styled';
 
 export const NewsPage = () => {
   const { t } = useTranslation();
   return (
-    <>
-      <h2>{t('News')}</h2>
+    <SC.Main>
+      <SC.Title>{t('News')}</SC.Title>
       <SearchBar />
       <NewsList />
-    </>
+    </SC.Main>
   );
 };
