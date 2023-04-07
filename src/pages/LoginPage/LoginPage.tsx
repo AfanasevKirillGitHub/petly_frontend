1422eba537b2b5c2fb50bc4f8bf904c84ecaf2f4
GeorgiyDer <106014816+GeorgiyDer@users.noreply.github.com>
2023-04-07 19:12:17 +0300

    чет накидал

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,11 @@
-import {LoginForm} from "../../components/LoginForm/LoginForm"
+import {LoginForm} from "../../components/LoginForm/LoginForm";
+import * as SC from './LoginPage.styled'
 
 export const LoginPage = () => {
 
   return (
-    <>
+    <SC.Main>
     <LoginForm />
-    </>
+    </SC.Main>
   );
 };
