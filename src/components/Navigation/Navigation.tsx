ce94a639c6737e72237e0b0bf2f322448463be96
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2023-04-30 18:36:31 +0300

    Update Navigation.tsx

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,43 +1,45 @@
 import { useTranslation } from 'react-i18next';
-
 import { NavLink } from 'react-router-dom';
-
 import { pages, authPages, userPage } from '../../helpers/pages';
 import { ChangeLngElem } from '../ChangeLngElem/ChangeLngElem';
 import { useAuth } from '../../hooks/useAuth';
-import * as SC from './Navigation.styled';
 import logo from '../../helpers/photos/petly.png';
+import * as SC from './Navigation.styled';
 
 export const Navigation = () => {
   const { t } = useTranslation();
   const { isLoggedIn } = useAuth();
+  
   return (
     <div>
       <SC.MainNav>
         <SC.NavList>
-        <NavLink to="/" style ={{textDecoration: 'none'}}>
-          <SC.Logo src = {logo}/>
-        </NavLink>
+          <NavLink to="/" style ={{textDecoration: 'none'}}>
+            <SC.Logo src = {logo}/>
+          </NavLink>
+
           {pages.map(({ href, name, id }) => (
             <SC.NavListItem key={id}>
               <SC.Link to={href}> {t(`navigation.${name}`)}</SC.Link>
             </SC.NavListItem>
           ))}
-          </SC.NavList>
-          <SC.NavListAuth>
-          {!isLoggedIn
-            ? authPages.map(({ href, name, id }) => (
-                <SC.NavListItemAuth key={id}>
-                  <SC.LinkAuth to={href}> {t(`navigation.${name}`)}</SC.LinkAuth>
-                </SC.NavListItemAuth>
-              ))
+        </SC.NavList>
+
+        <SC.NavListAuth>
+          {!isLoggedIn ?
+            authPages.map(({ href, name, id }) => (
+              <SC.NavListItemAuth key={id}>
+                <SC.LinkAuth to={href}> {t(`navigation.${name}`)}</SC.LinkAuth>
+              </SC.NavListItemAuth>
+            ))
             : userPage.map(({ href, name, id }) => (
-                <SC.NavListItemAuth key={id}>
-                  <SC.LinkAcc to={href}> {t(`navigation.${name}`)}</SC.LinkAcc>
-                </SC.NavListItemAuth>
-              ))}
-              <ChangeLngElem />
-            </SC.NavListAuth>
+              <SC.NavListItemAuth key={id}>
+                <SC.LinkAcc to={href}> {t(`navigation.${name}`)}</SC.LinkAcc>
+              </SC.NavListItemAuth>
+            ))
+          }
+          <ChangeLngElem />
+        </SC.NavListAuth>
       </SC.MainNav>
     </div>
   );
