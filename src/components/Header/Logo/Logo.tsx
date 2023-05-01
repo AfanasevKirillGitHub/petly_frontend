566b998bc0ddf697d0e3a7b8fc41aafe066a5cb4
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2023-05-01 20:59:32 +0300

    fix/logo functions

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -1,31 +1,11 @@
-import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../../helpers/photos/petly.png';
 import * as SC from './Logo.styled';
 
-interface IProps {
-    toggleBurgerMenu: () => void;
-}
-
-export const Logo = ({ toggleBurgerMenu }: IProps) => {
-    const [isDesktop, setIsDesktop] = useState(false);
-
-    const navigateByLogo = () => {
-        if (window.innerWidth >= 1440) {
-            return setIsDesktop(true);
-        };
-
-        return !isDesktop && closeBurgerMenu();
-    };
-
-    const closeBurgerMenu = () => {
-        toggleBurgerMenu();
-        document.body.style.overflow = '';
-    };
-    
+export const Logo = () => {
     return (
         <NavLink to="/" style ={{textDecoration: 'none'}}>
-            <SC.Logo src = {logo} onClick={navigateByLogo}/>
+            <SC.Logo src = {logo} />
         </NavLink>
     );
 };
