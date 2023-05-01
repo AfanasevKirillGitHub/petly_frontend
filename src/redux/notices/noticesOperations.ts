89c43469b09bfc163195923de8aec2a5508b0955
SPoluliakh <S_Poluliakh@ukr.net>
2023-05-01 23:00:09 +0300

    add hooks

--- src/redux/notices/noticesOperations.ts
+++ src/redux/notices/noticesOperations.ts
@@ -5,8 +5,8 @@ import { RootState } from '../store';
 export const noticesApi = createApi({
   reducerPath: 'notices',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:8080/api/notices',
-    // baseUrl: 'https://your-pets.onrender.com/api/notices',
+    // baseUrl: 'http://localhost:8080/api/notices',
+    baseUrl: 'https://your-pets.onrender.com/api/notices',
     prepareHeaders: (headers, { getState }) => {
       const token: string | null = (getState() as RootState).auth.token;
       if (token) {
@@ -35,7 +35,69 @@ export const noticesApi = createApi({
 
       invalidatesTags: ['notices'],
     }),
+    fetchById: builder.query<INotice, any>({
+      query: ({ lang = 'en', page = '1', limit = '20', id }) => ({
+        method: 'GET',
+        url: `/card/${id}?lang=${lang}&page=${page}&limit=${limit}`,
+      }),
+      providesTags: ['notices'],
+    }),
+    addNoticesToFavorite: builder.mutation<any, any>({
+      query: id => ({
+        method: 'POST',
+        url: `/favorites/${id}`,
+      }),
+
+      invalidatesTags: ['notices'],
+    }),
+    removeNoticesFromFavorite: builder.mutation<any, any>({
+      query: id => ({
+        method: 'DELETE',
+        url: `/favorites/${id}`,
+      }),
+
+      invalidatesTags: ['notices'],
+    }),
+    fetchFavorite: builder.query<INotice[] | [], any>({
+      query: ({ lang = 'en', page = '1', limit = '20' }) => ({
+        method: 'GET',
+        url: `/favorite?lang=${lang}&page=${page}&limit=${limit}`,
+      }),
+      providesTags: ['notices'],
+    }),
+    fetchOwn: builder.query<INotice[] | [], any>({
+      query: ({ lang = 'en', page = '1', limit = '20' }) => ({
+        method: 'GET',
+        url: `/own?lang=${lang}&page=${page}&limit=${limit}`,
+      }),
+      providesTags: ['notices'],
+    }),
+    fetchByCategory: builder.query<INotice[] | [], any>({
+      query: ({ lang = 'en', page = '1', limit = '20', category }) => ({
+        method: 'GET',
+        url: `/${category}?lang=${lang}&page=${page}&limit=${limit}`,
+      }),
+      providesTags: ['notices'],
+    }),
+    removeOwn: builder.mutation<any, any>({
+      query: id => ({
+        method: 'DELETE',
+        url: `/own/${id}`,
+      }),
+
+      invalidatesTags: ['notices'],
+    }),
   }),
 });
 
-export const { useFetchNoticesQuery, useAddNoticesMutation } = noticesApi;
+export const {
+  useFetchNoticesQuery,
+  useAddNoticesMutation,
+  useFetchByIdQuery,
+  useAddNoticesToFavoriteMutation,
+  useRemoveNoticesFromFavoriteMutation,
+  useFetchFavoriteQuery,
+  useFetchOwnQuery,
+  useFetchByCategoryQuery,
+  useRemoveOwnMutation,
+} = noticesApi;
