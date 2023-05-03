667b2725a00cfd1feb332a57c3bdf9a2fdd71913
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-05-03 22:07:53 +0300

    fix header styles

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -1,27 +1,37 @@
+import { ChangeLngElem } from '../../ChangeLngElem/ChangeLngElem';
 import * as SC from './Logo.styled';
 
 interface IProps {
-    toggleBurgerMenu: () => void;
+  toggleBurgerMenu: () => void;
 }
 
 export const Logo = ({ toggleBurgerMenu }: IProps) => {
-    const closerMenuByLogo = () => {
-        if (document.querySelector('#menu-container')?.classList.contains('is-open')) {
-            toggleBurgerMenu();
-        };
+  const closerMenuByLogo = () => {
+    if (
+      document.querySelector('#menu-container')?.classList.contains('is-open')
+    ) {
+      toggleBurgerMenu();
+    }
 
-        if (document.querySelector('#menu-container-for-tablet')?.classList.contains('is-open')) {
-            toggleBurgerMenu();
-        };
+    if (
+      document
+        .querySelector('#menu-container-for-tablet')
+        ?.classList.contains('is-open')
+    ) {
+      toggleBurgerMenu();
+    }
 
-        return;
-    };
+    return;
+  };
 
-    return (
-        <SC.LogoLink to="/" onClick={closerMenuByLogo}>
-            pe
-            <SC.LogoSpan>t</SC.LogoSpan>
-            ly
-        </SC.LogoLink>
-    );
+  return (
+    <SC.LogoWrapper>
+      <SC.LogoLink to="/" onClick={closerMenuByLogo}>
+        pe
+        <SC.LogoSpan>t</SC.LogoSpan>
+        ly
+      </SC.LogoLink>
+      <ChangeLngElem />
+    </SC.LogoWrapper>
+  );
 };
